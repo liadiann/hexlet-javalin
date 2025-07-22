195226efd6569b621915bcd5f0fc36f11a0ace56
Julia Kon <kon1976y@gmail.com>
2025-07-22 11:20:18 +0300

    add dinamic routes

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -13,6 +13,17 @@ public class HelloWorld {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name + "!");
         });
+
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Integer.class).get();
+            ctx.result("Course ID: " + id);
+        });
+
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            var userId = ctx.pathParamAsClass("id", Integer.class).get();
+            var postId = ctx.pathParamAsClass("postId", Integer.class).get();
+            ctx.result("User ID: " + userId + "\n" + "Post ID: " + postId);
+        });
         app.start(7070);
     }
 }
